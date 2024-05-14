2f9de83a625bd80fed3b6222797497a476231d7e
mulutu <mulutujackson@gmail.com>
2024-05-14 21:47:44 +0300

    First Commit

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -3,11 +3,14 @@ import { db } from '@/db'
 import { getUserSubscriptionPlan } from '@/lib/stripe'
 import { redirect } from 'next/navigation'
 import { SignedIn, auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
 
 const Page = async () => {
 
   const { userId } = auth();
   if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
 
   /*const dbUser = await db.user.findFirst({
     where: {
